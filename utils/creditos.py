a2abdf5a5ae963d3e29b42369287a8e494f268f4
Leandro Ornelas <leandro.nanndo@gmail.com>
2025-04-26 23:35:44 -0300

    inserindoCreditos

---
+++ utils/creditos.py
@@ -0,0 +1,21 @@
+creditos_html = """
+    <div style="
+        position: fixed; 
+        bottom: 10px; 
+        right: 10px; 
+        z-index: 9999; 
+        background-color: white; 
+        padding: 5px 10px; 
+        border-radius: 8px; 
+        box-shadow: 0 0 5px rgba(0,0,0,0.3);
+        font-size: 12px;
+        display: flex;
+        align-items: center;
+        gap: 5px;
+    ">
+        Desenvolvido por: Leandro Ornelas 
+        <a href="https://github.com/LeandroOrnelas" target="_blank" style="text-decoration: none; color: #333;">
+            <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" style="width:18; height:18px; vertical-align:middle;">
+        </a>
+    </div>
+"""
